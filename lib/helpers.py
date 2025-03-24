af7bd9b38cbb4f5967e5676804f3b9ee2594cd34
Jay Davis <jdlikesbsbl@gmail.com>
2025-03-24 12:53:06 -0500

    inputs name and age verification

--- lib/helpers.py
+++ lib/helpers.py
@@ -6,8 +6,31 @@ from debug import Madison
 from debug import Johnathan
 
 from colorama import Fore, Style, init
+from models.Customer import Customer
 
 init()
+
+def customer_name():
+    while True:
+        name = input("Enter name (1-10 characters): ").strip()
+        print(f"Raw input: {repr(name)}") 
+        try:
+            customer = Customer(name=name, age=25)
+            return customer
+        except (ValueError, TypeError) as e:
+            print(Fore.RED + str(e) + Style.RESET_ALL)
+            print(Fore.CYAN + "Please enter a valid name." + Style.RESET_ALL)
+
+def customer_age(customer):
+    while True:
+        age = input("Enter your age: ")
+        try:
+            customer.age = int(age)
+            customer.save()
+            return customer.age
+        except(ValueError, TypeError) as e:
+            print(Fore.RED + str(e) + Style.RESET_ALL)
+            print(Fore.CYAN + "Please enter a valid age." + Style.RESET_ALL)
 
 def age_checker():
     if Jay.age > 21:
--- lib/models/Customer.py
+++ lib/models/Customer.py
@@ -2,8 +2,8 @@ from models.__init__ import CONN, CURSOR
 import ipdb
 
 class Customer():
-    def __init__(self, name, age):
-        #self.id = id
+    def __init__(self, name, age, id=None):
+        self._id = id
         self.name = name
         self.age = age
     
@@ -25,7 +25,7 @@ class Customer():
     def name(self, value):
         if not isinstance(value, str):
             raise TypeError("Name has to be a string")
-        if 1 <= len(value) >= 10: 
+        if not (1 <= len(value) <= 10): 
             raise ValueError("Name has to be from 1-10 characters")
         self._name = value
     
@@ -37,7 +37,7 @@ class Customer():
     def age(self, value):
         if not isinstance(value, int):
             raise TypeError("Age has to be of type int")
-        if 1 <= value >= 122:
+        if not (1 <= value <= 122):
             raise ValueError("Enter a valid age")
         self._age = value
 
