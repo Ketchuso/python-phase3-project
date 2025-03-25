7dfefd07a4f33bb2599490ce545e8f58540ffd61
VictoriaCarvalho <vix219@icloud.com>
2025-03-25 11:14:39 -0500

    ADD: find_by and update to drink_orders

--- lib/models/Drink_Orders.py
+++ lib/models/Drink_Orders.py
@@ -21,5 +21,29 @@ class Drink_Orders():
         CURSOR.execute(sql)
         CONN.commit()
 
+    @classmethod
+    def find_by_id(cls, id):
+        sql = """
+            SELECT * FROM drink_orders
+            WHERE customer_id = ?;
+        """
+
+        row = CURSOR.execute(sql, (id,)).fetchall()
+        if row:
+            return cls.instance_from_row(row)
+        else:
+            return None
+    
+    
+    def update(self):
+        sql = """
+            UPDATE drink_orders
+            SET customer_name = ?,
+            WHERE customer_id = ?;
+        """
+        CURSOR.execute(sql, (self.name, self.id))
+        CONN.commit()
+
+
 
 
